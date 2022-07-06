1bb503c7dce248d147da5ed81b15d4c6ebef29e7
jihye.seong <jihye.seong@ipageon.com>
2022-07-06 19:58:17 +0900

    반복되는 CRUD 코드들..

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -30,6 +30,39 @@ class MemberJpaRepositoryTest {
     }
 
     @Test
-    void find() {
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("member!!!!!!");
+        // member 객체 생성 시 member 이름을 member1로 저장했다.
+        // 이후 영속성 컨텍스트에서 조회해서 이름을 member!!!!!! 으로 수정했다.
+        // 이 때, 영속성 컨텍스트에서는 member 객체의 이름이 바꼈다는 것일 인지(더티체킹)하고
+        // 트랜잭션이 커밋될 때, 바뀐 값으로 DB에 쿼리를 날린다.
+
+        /*// 리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);*/
     }
 }
