aa4d1edfd9858dffd6f5f2edad267806b65f482e
jihye.seong <jihye.seong@ipageon.com>
2022-07-21 12:50:14 +0900

    web 확장 domain Class Convertor 사용한 RestAPI 요청 파라미터 객체로 받기, 단순 조회용으로만 사용하자!

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,35 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    // 일반 RestAPI로 id 값 요청
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    // 도메인 클래스 컨버터 사용! -> Member 객체로 받을 수 있음!
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
