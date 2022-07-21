111f2a5f865a94bfdf70b818b90d6209b8a9932f
jihye.seong <jihye.seong@ipageon.com>
2022-07-21 13:54:27 +0900

    웹 페이징 처리와 정렬 처리, 스프링부트 페이지 디폴트 설정 바꾸기 등

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -28,8 +32,23 @@ public class MemberController {
         return member.getUsername();
     }
 
+    // web paging and ordering
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+
+        // 엔티티를 노출하면 절 대 노출하면 안된다.
+        // 엔티티를 DTO로 바꿔주자!
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
+
 }
