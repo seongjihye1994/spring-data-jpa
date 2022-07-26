3048066a01b3f4a6d4e77126232ed5e5fc9ec2fb
jihye.seong <jihye.seong@ipageon.com>
2022-07-26 17:37:17 +0900

    Spring Data JPA에서 지원하는 Specifications 기능, 하지만 복잡하므로 사용하지 말자.

---
+++ src/main/java/study/datajpa/repository/MemberSpec.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+
+import javax.persistence.criteria.*;
+
+public class MemberSpec {
+
+    public static Specification<Member> teamName(final String teamName) {
+        return new Specification<Member>() {
+            @Override
+            public Predicate toPredicate(Root<Member> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+
+                if (StringUtils.isEmpty(teamName)) {
+                    return null;
+                }
+
+                Join<Member, Team> t = root.join("team", JoinType.INNER);// 회원과 조인
+
+                return builder.equal(t.get("name"), teamName); // where 문이 만들어진다.
+
+            }
+        };
+    }
+
+    public static Specification<Member> username(final String username) {
+        return (Specification<Member>) (root, query, builder) ->
+            builder.equal(root.get("username"), username);
+    }
+}
